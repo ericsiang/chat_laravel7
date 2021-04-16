17f205c0d5b9db7e84e1da033497a7bfcfde79d3
eric <ericchang60917@gail.com>
2021-04-16 16:08:50 +0800

    add laravel 7 init and add chat blade

---
+++ resources/views/chat.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+</head>
+<body>
+
+    <div class="container">
+        <div class="row" id="app">
+
+            <ul class="list-group offset-4 col-4">
+                <li class="list-group-item active" aria-current="true">Chat room</li>
+                <li class="list-group-item">A second item</li>
+                <li class="list-group-item">A third item</li>
+                <li class="list-group-item">A fourth item</li>
+                <li class="list-group-item">And a fifth one</li>
+                <input type="text" class="form-control" placeholder="Type your message here...">
+            </ul>
+        </div>
+    </div>
+    <script src="{{ asset('js/app.js') }}"></script>
+</body>
+</html>
